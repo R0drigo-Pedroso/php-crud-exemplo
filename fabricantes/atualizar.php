5e02a6268f8489a8085c486b688c5ffba05d6183
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-05-20 08:34:15 -0300

    Update atualizar.php

--- fabricantes/atualizar.php
+++ fabricantes/atualizar.php
@@ -6,7 +6,18 @@
     $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 
     $nomefabricante = lerUmFabricante($conexao, $id);
+
+    if (isset ($_POST ['atualizar'])) {
+        $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+        
+        atualizarFabricante($conexao, $id, $nome);
+
+        header('Location: listar.php');
+    }
+
+
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
