d8571f2a060304f4d7b73607529b9415a6dccdb2
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-05-27 09:59:45 -0300

    Update inserir.php

--- produtos/inserir.php
+++ produtos/inserir.php
@@ -26,9 +26,6 @@ if(isset($_POST['inserir'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Produtos - Inserir</title>
 
-    <!-- CSS -->
-    <link rel="stylesheet" href="../css/estilo.css">
-
 </head>
 <body>
     <div>
